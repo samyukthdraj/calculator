82d27c951b480b36840252c9e185ea9a2583f82e
samyukthdraj <samyukthdharamarajan2002@gmail.com>
2023-11-06 00:36:13 +0400

    Names given and then called

--- functions_output.py
+++ functions_output.py
@@ -1,2 +1,8 @@
 print("Hello World\n")
 print("hi")
+def names_function(fname,lname):
+    first=fname.title()
+    last=lname.title()
+    return  f"Welcome {first} {last}"
+
+names_function("Samyukth", "Dharmarajan")
